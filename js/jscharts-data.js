5d82553e8b112ba7bc6221c6c3b42ce56831d11d
AdamM-AU <70795583+AdamM-AU@users.noreply.github.com>
2020-11-27 23:20:12 +1100

    Completed

--- js/jscharts-data.js
+++ js/jscharts-data.js
@@ -15,7 +15,7 @@ function JSFetchGraph(GRAPH) {
 						label: '# of Commits',
 						barPercentage: 1,
 						minBarLength: 2,
-						data: [140, 8, 29, 5, 0, 26],
+						data: [149, 19, 29, 5, 0, 27],
 						backgroundColor: [
 							'rgba(255, 99, 132, 0.2)',
 							'rgba(54, 162, 235, 0.2)',
